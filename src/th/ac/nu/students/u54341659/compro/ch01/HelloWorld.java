d86d0d42958cb9c9df229480265d96e729f34d29
happi2goal <l3iskids@hotmail.com>
2012-07-12 00:06:56 +0700

    เพิ่ม Programming is Fun

--- src/th/ac/nu/students/u54341659/compro/ch01/HelloWorld.java
+++ src/th/ac/nu/students/u54341659/compro/ch01/HelloWorld.java
@@ -4,5 +4,6 @@ package th.ac.nu.students.u54341659.compro.ch01;
 public class HelloWorld {
     public static void main (String [] args){
         System.out.println("Hello, World");
+        System.out.print("Programming is Fun !!");
     }
 }
