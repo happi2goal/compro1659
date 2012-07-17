9f587242bdf2744844688b6237ce88ed28248a5b
happi2goal <l3iskids@hotmail.com>
2012-07-17 16:47:28 +0700

    Compro ch03

---
+++ src/th/ac/nu/students/u54341659/compro/ch03/LabelStatement.java
@@ -0,0 +1,13 @@
+
+package th.ac.nu.students.u54341659.compro.ch03;
+
+public class LabelStatement {
+
+    public static void main(String[] args) {
+        first : {
+             System.out.print("Hello, ");   
+             here : System.out.println("World!");
+        }
+         
+    }
+}
